b2e159a91f7be2c920788a305dbde80cd66710f9
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2023-12-19 17:05:58 -0600

    Create functions.php

---
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Theme functions and definitions.
+ *
+ * For additional information on potential customization options,
+ * read the developers' documentation:
+ *
+ * https://developers.elementor.com/docs/hello-elementor-theme/
+ *
+ * @package HelloElementorChild
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
+
+/**
+ * Load child theme scripts & styles.
+ *
+ * @return void
+ */
+function hello_elementor_child_scripts_styles() {
+
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		HELLO_ELEMENTOR_CHILD_VERSION
+	);
+	wp_enqueue_script(
+		'hello-elementor-child-script',
+		get_stylesheet_directory_uri() . '/script.js',
+		[
+			'jquery',
+		],
+		HELLO_ELEMENTOR_CHILD_VERSION,
+		true
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
